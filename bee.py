4dc51cd7f85faa5e03d6ba14c6d5ff37606c77db
Jakub <jkbpawlowski791@gmail.com>
2023-12-09 22:23:44 +0100

    macierz kosztu i produktu

--- bee.py
+++ bee.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
-
+from users import *
+from obj import *
 # do wytworzenia macierzy kosztu transportu macierz = [[random.uniform(0.0, 10.0) for i in range(producents)] for i in range(customers)]
 # do wytworzenia ilosci produktow lista_produktow = [random.randint(max_demand, 100) for i in range(n)]
 # do wytworzenia cen produktow cena_produktow = [round(random.uniform(0.0, 100.0), 2) for i in range(n)]
@@ -9,61 +10,8 @@ import random
 # pierw stworz liste zapotrzebowan, zsumuj zapotrzebowanie wszystkich klientow, a nastepnie stworz liste produktow producentow gdzie dolny zakres
 # to zsumowane zaptorzebowanie klientow
 
-class Producents():
-    def __init__(self, id, quantity, price):
-        self.id = id
-        self.quantity = quantity
-        self.price = price
-
-class Customers():
-    def __init__(self, id, demand):
-        self.id = id
-        self.demand = demand
-
-class Bee():
-    def __init__(self, producents, customers, distance, price):
-        self.producents = producents
-        self.customers = customers
-        self.distance = distance
-        self.price = price
-        self.product = self.generate_solution(producents, customers, distance, price)
-    
-
-        
-    def generate_solution(self, producents, customers, distance, price):
-        number_producents = len(producents[0])
-        number_customers = len(customers[0])
-        cost = [[0] * number_customers for _ in range(number_producents)]
-        product = [[0] * number_customers for _ in range(number_producents)]        
-
-        for i in range(number_producents):
-            for j in range(number_customers):
-                if (customers[i][j] > 0):
-                    cost[i][j] = round(price[i][j] * customers[i][j] + distance[i][j] * 0.45, 2)
-                    product[i][j] = customers[i][j]
-                    
-                else:
-                    cost[i][j] = 0
-              
-        return cost, product
 
 
-        
-
-producentA = Producents(1, [random.randint(50, 100) for i in range(4)], [round(random.uniform(0.0, 100.0), 2) for i in range(4)])
-producentB = Producents(2, [random.randint(50, 100) for i in range(4)], [round(random.uniform(0.0, 100.0), 2) for i in range(4)])
-producentC = Producents(3, [random.randint(50, 100) for i in range(4)], [round(random.uniform(0.0, 100.0), 2) for i in range(4)])
-producentD = Producents(4, [random.randint(50, 100) for i in range(4)], [round(random.uniform(0.0, 100.0), 2) for i in range(4)])
-
-customerA = Customers(1, [random.randint(0, 50) for i in range(4)])
-customerB = Customers(2, [random.randint(0, 50) for i in range(4)])
-customerC = Customers(3, [random.randint(0, 50) for i in range(4)])
-customerD = Customers(4, [random.randint(0, 50) for i in range(4)])
-
-matrix_producent = [producentA.quantity, producentB.quantity, producentC.quantity, producentD.quantity]
-matrix_customer = [customerA.demand, customerB.demand, customerC.demand, customerD.demand]
-matrix_price = [producentA.price, producentB.price, producentC.price, producentD.price]
-distance = [[round(random.uniform(0.0, 10.0), 2) for i in range(4)] for j in range(4)]
 
 bee1 = Bee(matrix_producent, matrix_customer, distance, matrix_price)
 cost, product = bee1.generate_solution(bee1.producents, bee1.customers, bee1.distance, bee1.price)
