c02d8072b17af430e58fff143dd1e873ff876d98
Jakub <jkbpawlowski791@gmail.com>
2023-12-09 22:27:59 +0100

    pliki naglowkowe

--- obj.py
+++ obj.py
@@ -1,5 +1,18 @@
 import random
 
+# do wytworzenia macierzy kosztu transportu macierz = [[random.uniform(0.0, 10.0) for i in range(producents)] for i in range(customers)]
+# do wytworzenia ilosci produktow lista_produktow = [random.randint(max_demand, 100) for i in range(n)]
+# do wytworzenia cen produktow cena_produktow = [round(random.uniform(0.0, 100.0), 2) for i in range(n)]
+# do wytworzenia zapotrzebowania klienta zapotrzebowanie = [random.randint(0, max_demand) for i in range(n)]
+
+# pierw stworz liste zapotrzebowan, zsumuj zapotrzebowanie wszystkich klientow, a nastepnie stworz liste produktow producentow gdzie dolny zakres
+# to zsumowane zaptorzebowanie klientow
+
+#przykladowy producent 
+#producent1 = Producents(1, [random.randint(0, 100) for i in range(5)], [round(random.uniform(0.0, 100.0), 2) for i in range(5)])
+# wynik 1 [52, 60, 72, 49, 73] [30.95, 28.44, 36.31, 82.93, 61.77]
+#gdzie odpowiednio mamy id producenta, liste ilosci produktow produkowanych oraz liste cen dla danego produktu
+
 
 class Producents():
     def __init__(self, id, quantity, price):
