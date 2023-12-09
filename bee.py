13f6ca02af9819750dae94332662c1bd9d6509fa
Jakub <jkbpawlowski791@gmail.com>
2023-12-09 19:32:42 +0100

    commit

---
+++ bee.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+
+# do wytworzenia ilosci produktow lista_produktow = [random.randint(0, 100) for i in range(n)]
+# do wytworzenia cen produktow cena_produktow = [round(random.uniform(0.0, 100.0), 2) for i in range(n)]
+
+class Producents():
+    def __init__(self, id, quantity, price):
+        self.id = id
+        self.quantity = quantity
+        self.price = price
+
+class Customers():
+    def __init__(self, id, demand):
+        self.id = id
+        self.demand = demand
+
+        
+#przykladowy producent 
+#producent1 = Producents(1, [random.randint(0, 100) for i in range(5)], [round(random.uniform(0.0, 100.0), 2) for i in range(5)])
+# wynik 1 [52, 60, 72, 49, 73] [30.95, 28.44, 36.31, 82.93, 61.77]
+#gdzie odpowiednio mamy id producenta, liste ilosci produktow produkowanych oraz liste cen dla danego produktu
+
+
