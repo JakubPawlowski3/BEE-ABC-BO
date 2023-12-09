e82fbce8c729a2c8cdb92d96573bbd0f49585452
Jakub <jkbpawlowski791@gmail.com>
2023-12-09 19:41:10 +0100

    do_wytworzenia_

--- bee.py
+++ bee.py
@@ -1,8 +1,9 @@
 import numpy as np
 import random
 
-# do wytworzenia ilosci produktow lista_produktow = [random.randint(0, 100) for i in range(n)]
+# do wytworzenia ilosci produktow lista_produktow = [random.randint(max_demand, 100) for i in range(n)]
 # do wytworzenia cen produktow cena_produktow = [round(random.uniform(0.0, 100.0), 2) for i in range(n)]
+# do wytworzenia zapotrzebowania klienta zapotrzebowanie = [random.randint(0, max_demand) for i in range(n)]
 
 class Producents():
     def __init__(self, id, quantity, price):
